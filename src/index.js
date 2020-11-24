c76a0ff419c87af1344966defbcdbae54f28b22b
Yiunhi-LeeWoonghee <yi.unhi.yi@gmail.com>
2020-11-24 23:57:09 +0900

    2020/11/24 react第10講まで

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // import react
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 // Create a new component. This component should produce some HTML
 // 새로운 컴포넌트를 만든다. 이 컴포넌트는 몇몇 HTML을 생산한다.
@@ -9,4 +10,4 @@ const App = function() {
 
 // Take this component's generated HTML and put it on the page (in the DOM)
 // 이 컴포넌트가 만든 HTML을 가져가고 페이지에 반영한다. (DOM 안에)
-React.render(App);
+ReactDOM.render(<App />);
